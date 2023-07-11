5b9217dbf948047d4025f898ddb326358d914a44
dmitriy.balashov <reaver_std@mail.ru>
2023-07-11 21:30:33 +0500

    refactor playlist class

--- src/playlist.py
+++ src/playlist.py
@@ -1,4 +1,4 @@
-import json
+import datetime
 
 import isodate
 
@@ -9,29 +9,26 @@ class PlayList:
 
     def __init__(self, playlist_id):
         youtube = Channel.get_service()
-        playlist = youtube.playlistItems().list(playlistId=playlist_id,
-                                                part='contentDetails, snippet'
-                                                ).execute()
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist['items']]
+        playlist = youtube.playlists().list(id=playlist_id,
+                                            part='contentDetails, snippet').execute()['items'][0]['snippet']
+        playlist_items = youtube.playlistItems().list(playlistId=playlist_id,
+                                                      part='contentDetails, snippet'
+                                                      ).execute()['items']
+        video_ids = [video['contentDetails']['videoId'] for video in playlist_items]
         self.video_response = youtube.videos().list(part='contentDetails,statistics',
                                                     id=','.join(video_ids)
-                                                    ).execute()
-        self.title = playlist['items'][0]['snippet']['title'][:24]
+                                                    ).execute()['items']
+        self.title = playlist['title']
         self.url = "https://www.youtube.com/playlist?list=" + playlist_id
 
     @property
     def total_duration(self):
-        total = isodate.parse_duration("PT0M0S")
-        for video in self.video_response['items']:
-            iso_8601_duration = video['contentDetails']['duration']
-            duration = isodate.parse_duration(iso_8601_duration)
-            total += duration
-        return total
+        temp = [item['contentDetails']['duration'] for item in self.video_response]
+        time_delta_sec = sum([isodate.parse_duration(elem).seconds for elem in temp])
+        return datetime.timedelta(seconds=time_delta_sec)
 
     def show_best_video(self):
-        max_likes = 0
-        for video in self.video_response['items']:
-            if max_likes < int(video['statistics']['likeCount']):
-                max_likes = int(video['statistics']['likeCount'])
-                max_likes_video = video['id']
+        max_likes = max([int(item['statistics']['likeCount']) for item in self.video_response])
+        max_likes_video = [video['id'] for video in self.video_response if
+                           video['statistics']['likeCount'] == str(max_likes)][0]
         return "https://youtu.be/" + max_likes_video
